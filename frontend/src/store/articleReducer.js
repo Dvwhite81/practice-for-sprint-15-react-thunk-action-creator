dcfe458a92a9d42c852d7b0e9f0186ba3d383a19
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-02 14:25:31 -0400

    Complete

--- frontend/src/store/articleReducer.js
+++ frontend/src/store/articleReducer.js
@@ -1,9 +1,7 @@
-import articles from '../data/data.json';
-
 const LOAD_ARTICLES = 'article/loadArticles';
 const ADD_ARTICLE = 'article/addArticle';
 
-export const loadArticles = () => {
+export const loadArticles = (articles) => {
   return {
     type: LOAD_ARTICLES,
     articles
@@ -28,6 +26,25 @@ const articleReducer = (state = initialState, action) => {
     default:
       return state;
   }
+};
+
+export const fetchArticles = () => async dispatch => {
+  const response = await fetch('/api/articles');
+  const articles = await response.json();
+  dispatch(loadArticles(articles));
+};
+
+export const writeArticle = (payload) => async dispatch => {
+  const response = await fetch('/api/articles', {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json",
+    },
+    body: JSON.stringify(payload)
+  });
+
+  const article = await response.json();
+  dispatch(addArticle(article));
 };
 
 export default articleReducer;
